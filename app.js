ff491f0d5805888703c49a4279f01e19423b8cd5
Abu Md Nayeem Khan <abumdnayeemkhan@gmail.com>
2022-09-10 18:06:26 +0600

    ES6 decision making

---
+++ app.js
@@ -0,0 +1,11 @@
+//==========Disition Making==========//
+const nayeem = {dress:"t shirt", color:'Green', brand:'Van Plaza'}
+if(nayeem['color'] == "red"){
+    console.log("Nayeems wearring red color t shirt");
+} else if (nayeem['color'] == "green") {
+    console.log("Nayeems wearring green color t shirt");
+} else if (nayeem['color'] == "blue") {
+    console.log("Nayeems wearring blue color t shirt");
+} else {
+    console.log("Color is not found but his favorite color is Maroon.");
+}
